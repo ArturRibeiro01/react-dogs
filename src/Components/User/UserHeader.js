dde5bd0d574d3ec5637bb3b1342b1a73f3d91e33
ArturRibeiro01 <profissional.arturrc@gmail.com>
2020-11-24 17:19:46 -0300

    Mobile Menu

--- src/Components/User/UserHeader.js
+++ src/Components/User/UserHeader.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import UserHeaderNav from './UserHeaderNav'
 import styles from './UserHeader.module.css'
+import { useLocation } from 'react-router-dom';
 
 const UserHeader = () => {
+
+    const [title, setTitle] = React.useState('');
+    const location = useLocation();
+
+    React.useEffect(() =>{
+        const {pathname} = location;
+        switch(pathname) {
+            case '/conta/postar' : setTitle('Poste sua foto'); break;
+            case '/conta/estatisticas': setTitle('Estatísticas'); break;
+            default : setTitle('Minha conta')
+        };
+    },[location]);
+
     return (
         <header className={styles.header}>
-            <h1 className="title"> Titulo</h1>
+            <h1 className="title">{title}</h1>
             <UserHeaderNav />
         </header>
     )
--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -6,14 +6,30 @@ import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg';
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg';
 import {ReactComponent as Sair} from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import { useMedia } from '../../Hooks/useMedia';
 
 
 const UserHeaderNav = () => {
-    const [mobile, setMobile] = React.useState(null);
-    const {userLogout} = React.useContext(UserContext)
+    const {userLogout} = React.useContext(UserContext);
+    const mobile = useMedia('(max-width: 40rem)');
+    const [mobileMenu, setMobileMenu] = React.useState(false);
 
     return (
-        <nav className={styles.nav}>
+        <>
+        {mobile && (
+            <button 
+                aria-label ="Menu"
+                className={`
+                    ${styles.mobileButton}
+                    ${ mobileMenu && styles.mobileButtonActive}
+                `}
+                onClick={()=> setMobileMenu(!mobileMenu)
+                }>
+            </button>
+        )}
+        <nav className={`
+            ${mobile ? styles.navMobile : styles.nav} 
+            ${mobileMenu}`}>
             <NavLink to="/conta" end activeClassName={styles.active}>
                 <MinhasFotos/>
                 {mobile && 'Minhas Fotos'}
@@ -28,12 +44,13 @@ const UserHeaderNav = () => {
                 <AdicionarFoto/>
                 {mobile && 'Adicionar Foto'}
             </NavLink>
+
             <button onClick ={userLogout}>
                 <Sair/>
                 {mobile && 'Sair'}
             </button>
-
         </nav>
+        </>
     )
 }
 
